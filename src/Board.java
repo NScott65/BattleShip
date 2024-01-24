0880a2da98a2ee9a811e8b6c7a30c5f0ea252608
NScott65 <144457750+NScott65@users.noreply.github.com>
2024-01-24 09:45:27 -0500

    Update Board.java

--- src/Board.java
+++ src/Board.java
@@ -1,27 +1,38 @@
 public class Board {
     private static String[][] squares;
 
-    public static void main(String[] args) {
-
-
-
-
-    }
-
     public Board() {
         squares = new String[10][10];
-    }
 
-    public String toString(){
-        String value = " ";
         for(int r = 0; r < squares.length; r++){
             //Inner for loop accesses each column in the current row
             for(int c = 0; c < squares[0].length; c++){
                 squares[r][c] = "-";
-                System.out.print(squares[r][c] + " ");
             }
-            System.out.println();
         }
-         return value;
+    }
+
+    public String toString(){
+        String value = "";
+        for(int r = 0; r < squares.length; r++){
+            //Inner for loop accesses each column in the current row
+            for(int c = 0; c < squares[0].length; c++){
+                value += squares[r][c] + " ";
+            }
+            value += "\n";
+        }
+        return value;
+    }
+
+    public boolean addShip(int row, int col, int len, boolean horizontal ){
+        boolean val = false;
+
+        if(row <= 10 && col <= 10 && row >= 1 && col >= 1 && len >= 2 && len <= 5 ){
+
+        }else{
+            
+        }
+
+        return val;
     }
 }
