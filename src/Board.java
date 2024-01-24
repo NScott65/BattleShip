0ae58d45d1b814249268fa9e541c76a3677b2a28
NScott65 <scott.613865@swl.k12.oh.us>
2024-01-23 21:31:16 -0500

    h

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Board {
+public class Board {
+    private static String[][] squares;
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+    public Board() {
+        squares = new String[10][10];
+    }
+
+    public String toString(){
+        String value = " ";
+        for(int r = 0; r < squares.length; r++){
+            //Inner for loop accesses each column in the current row
+            for(int c = 0; c < squares[0].length; c++){
+                squares[r][c] = "-";
+                System.out.print(squares[r][c] + " ");
+            }
+            System.out.println();
+        }
+         return value;
+    }
 }
