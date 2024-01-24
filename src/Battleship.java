6581ab55d11c692a8f5bb8797d4d0b2d20ee1019
NScott65 <144457750+NScott65@users.noreply.github.com>
2024-01-24 17:19:31 -0500

    Update Battleship.java

--- src/Battleship.java
+++ src/Battleship.java
@@ -1,6 +1,8 @@
 public class Battleship {
     static Board gameBoard = new Board();
     public static void main(String[] args) {
+
+        gameBoard.addShip(3,4,2,true);
         System.out.println(gameBoard.toString());
     }
 }
