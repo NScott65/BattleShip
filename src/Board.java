abd07db72e32c76373908a07b4b2389074504b0f
NScott65 <144457750+NScott65@users.noreply.github.com>
2024-01-24 17:19:12 -0500

    Update Board.java

--- src/Board.java
+++ src/Board.java
@@ -27,10 +27,18 @@ public class Board {
     public boolean addShip(int row, int col, int len, boolean horizontal ){
         boolean val = false;
 
+
         if(row <= 10 && col <= 10 && row >= 1 && col >= 1 && len >= 2 && len <= 5 ){
+            if(horizontal){
+                for(int r = 0; r == row - 1; r++){
+                    for(int c = (0 - row) - 1;c <= (col + (len - 1)) - 1 && c >= col - 1; c++) {
+                        squares[r][c] = "b";
+                    }
+                }
+            }
 
         }else{
-            
+            System.out.println("Your input was invalid. Can't run.");
         }
 
         return val;
