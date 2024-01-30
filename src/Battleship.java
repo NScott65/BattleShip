a43a8c3cf0b6f03f2cc04f6566792e813acbc8b4
NScott65 <144457750+NScott65@users.noreply.github.com>
2024-01-30 09:44:40 -0500

    Update Battleship.java

--- src/Battleship.java
+++ src/Battleship.java
@@ -1,11 +1,24 @@
+import java.util.Scanner;
 public class Battleship {
+
     static Board gameBoard = new Board();
+
+    static Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
+        String choice = "";
+        boolean done = false;
+        int addRow = 0;
+        int addCol = 0;
+        int addLen = 0;
 
-        gameBoard.addShip(3,4,2,true);
-        System.out.println(gameBoard.toString());
-        gameBoard.addShip(6,6,4,false);
-
-        System.out.println(gameBoard.toString());
+        do{
+            choice = Helper.getNonZeroLengString(scan, "Would you like to \"a\" add a ship, \"b\" see the board, \"p\" to play, or \"q\" to quit.")
+            if(choice.equalsIgnoreCase("a")){
+                addRow = Helper.getRangedInt(scan, "Enter a Row(0-9): ",0,9);
+                addCol = Helper.getRangedInt(scan, "Enter a Column(0-9): ",0,9);
+                addLen = Helper.getRangedInt(scan, "Enter a ship Length(2-9): ",2,9);
+            }
+        }while(!done);
     }
 }
